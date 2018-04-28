8e9238ca085114d0bd8bd60e130e75a8ab042843
erezt.dev <erezt.dev@gmail.com>
2018-04-28 21:02:57 +0300

    Weather API needs city ID, built parsing with a Node API backEnd created (separate repo)

---
+++ src/components/Weather.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>
+            WEATHER
+        </h1>
+        <button @click="getCity">con city</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data(){
+            return{
+                cities:'',
+                cityId: ''
+            }
+
+        },
+        name: "Weather",
+        methods:{
+            allCities(){
+                axios.get('http://localhost:3000/allCities')
+                    .then(function (response) {
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+            getCityId(){
+                axios.post('http://localhost:3000/city',
+                this.name, // the data to post
+                { headers: {
+                        'Content-type': 'application/x-www-form-urlencoded',
+                    }
+                }).then(response => {
+                    console.log(response);
+                }).catch(function (error) {
+                    console.log(error);
+                });
+
+            }
+        },
+        mounted(){
+           this.allCities();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
